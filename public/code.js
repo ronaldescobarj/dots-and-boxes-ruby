860b1c43e3ae7081599f7a86510cf0c31b8a9ce9
Ronald Escobar <ronaldescobarj@gmail.com>
2018-11-03 19:10:48 -0400

    refactorized code.js

--- public/code.js
+++ public/code.js
@@ -1,11 +1,10 @@
 turns = ["#354698", "#C42615", "#EAA81D", "#86B71B", "#7A3AB9"]
-gray = "#7f7f7f";
 maxTurnsLength = 2;
 index = 0
 
 function markLine(id) {
+    let gray = "#7f7f7f";
     let line = document.getElementById(id);
-    console.log(line.getAttribute("stroke"))
     if (line.getAttribute("stroke") == gray) {
         line.setAttribute("stroke", turns[index])
         line.style.opacity = 1;
@@ -19,62 +18,77 @@ function nextTurn() {
         index = 0;
 }
 
-function checkSquare(id){
+function checkSquare(id) {
     id = id.substr(1);
     coordinates = id.split("_");
-    coords = { x1: parseInt(coordinates[0]),
+    coords = {
+        x1: parseInt(coordinates[0]),
         y1: parseInt(coordinates[1]),
         x2: parseInt(coordinates[2]),
-        y2: parseInt(coordinates[3])};
-
+        y2: parseInt(coordinates[3])
+    };
     horizontal = coords.y1 == coords.y2;
     let addedSquare = false;
-    if (horizontal) {
-        if (coords.y2 != 500) {
-            if (checkDownside(coords)) {
-                markId = generateMarkId(coords, 50, 50);
-                var mark = document.getElementById(markId)
-                mark.style.visibility = "visible";
-                mark.setAttribute("stroke", turns[index]);
-                mark.setAttribute("fill", turns[index]);
-                addedSquare = true;
-            }
-        }
-        if (coords.y1 != 100) {
-            if (checkUpside(coords)) {
-                markId = generateMarkId(coords, 50, -50);
-                var mark = document.getElementById(markId)
-                mark.style.visibility = "visible";
-                mark.setAttribute("stroke", turns[index]);
-                mark.setAttribute("fill", turns[index]);
-                addedSquare = true;
-            }
-        }
-    }
-    else {
-        if (coords.x2 != 500) {
-            if (checkRight(coords)) {
-                markId = generateMarkId(coords, 50, 50);
-                var mark = document.getElementById(markId)
-                mark.style.visibility = "visible";
-                mark.setAttribute("stroke", turns[index]);
-                mark.setAttribute("fill", turns[index]);
-                addedSquare = true;
-            }
-        }
-        if (coords.x1 != 100) {
-            if (checkLeft(coords)) {
-                markId = generateMarkId(coords, -50, 50);
-                var mark = document.getElementById(markId)
-                mark.style.visibility = "visible";
-                mark.setAttribute("stroke", turns[index]);
-                mark.setAttribute("fill", turns[index]);
-                addedSquare = true;
-            }
-        }
-    }
+    if (horizontal)
+        addedSquare = checkSquareHorizontal(coords)
+    else
+        addedSquare = checkSquareVertical(coords)
     if (!addedSquare)
         nextTurn();
+}
+
+function checkSquareHorizontal(coords) {
+    let addedSquare = false
+    // if the line isnt the last
+    if (coords.y2 != 500) {
+        if (checkDownside(coords)) {
+            markId = generateMarkId(coords, 50, 50);
+            var mark = document.getElementById(markId)
+            mark.style.visibility = "visible";
+            mark.setAttribute("stroke", turns[index]);
+            mark.setAttribute("fill", turns[index]);
+            addedSquare = true;
+        }
+    }
+    // if the line isnt the first
+    if (coords.y1 != 100) {
+        if (checkUpside(coords)) {
+            markId = generateMarkId(coords, 50, -50);
+            var mark = document.getElementById(markId)
+            mark.style.visibility = "visible";
+            mark.setAttribute("stroke", turns[index]);
+            mark.setAttribute("fill", turns[index]);
+            addedSquare = true;
+        }
+    }
+    return addedSquare
+}
+
+function checkSquareVertical(coords) {
+    let addedSquare = false
+    // if the line isnt the last
+    if (coords.x2 != 500) {
+        if (checkRight(coords)) {
+            markId = generateMarkId(coords, 50, 50);
+            var mark = document.getElementById(markId)
+            mark.style.visibility = "visible";
+            mark.setAttribute("stroke", turns[index]);
+            mark.setAttribute("fill", turns[index]);
+            addedSquare = true;
+        }
+    }
+    // if the line isnt the first
+    if (coords.x1 != 100) {
+        if (checkLeft(coords)) {
+            markId = generateMarkId(coords, -50, 50);
+            var mark = document.getElementById(markId)
+            mark.style.visibility = "visible";
+            mark.setAttribute("stroke", turns[index]);
+            mark.setAttribute("fill", turns[index]);
+            addedSquare = true;
+        }
+    }
+    return addedSquare
 }
 
 function checkDownside(coords) {
@@ -88,9 +102,7 @@ function checkDownside(coords) {
     tempCoords.x1 += 100;
     tempCoords.x2 += 100;
     neighbor3 = generateId(tempCoords);
-    return (document.getElementById(neighbor1).style.opacity == 1 &&
-    document.getElementById(neighbor2).style.opacity == 1 &&
-    document.getElementById(neighbor3).style.opacity == 1)
+    return allNeighborsAreFill(neighbor1, neighbor2, neighbor3)
 }
 
 function checkUpside(coords) {
@@ -104,9 +116,7 @@ function checkUpside(coords) {
     tempCoords.x1 += 100;
     tempCoords.x2 += 100;
     neighbor3 = generateId(tempCoords);
-    return (document.getElementById(neighbor1).style.opacity == 1 &&
-        document.getElementById(neighbor2).style.opacity == 1 &&
-        document.getElementById(neighbor3).style.opacity == 1)
+    return allNeighborsAreFill(neighbor1, neighbor2, neighbor3)
 }
 
 function checkLeft(coords) {
@@ -120,9 +130,7 @@ function checkLeft(coords) {
     tempCoords.y1 += 100;
     tempCoords.y2 += 100;
     neighbor3 = generateId(tempCoords);
-    return (document.getElementById(neighbor1).style.opacity == 1 &&
-        document.getElementById(neighbor2).style.opacity == 1 &&
-        document.getElementById(neighbor3).style.opacity == 1)
+    return allNeighborsAreFill(neighbor1, neighbor2, neighbor3)
 }
 
 function checkRight(coords) {
@@ -136,9 +144,13 @@ function checkRight(coords) {
     tempCoords.y1 += 100;
     tempCoords.y2 += 100;
     neighbor3 = generateId(tempCoords);
-    return (document.getElementById(neighbor1).style.opacity == 1 &&
-        document.getElementById(neighbor2).style.opacity == 1 &&
-        document.getElementById(neighbor3).style.opacity == 1)
+    return allNeighborsAreFill(neighbor1, neighbor2, neighbor3)
+}
+
+function allNeighborsAreFill(n1, n2, n3) {
+    return document.getElementById(n1).style.opacity == 1 &&
+        document.getElementById(n2).style.opacity == 1 &&
+        document.getElementById(n3).style.opacity == 1
 }
 
 function generateMarkId(obj, x, y) {
