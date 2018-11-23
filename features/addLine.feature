57d70b415718a87a411a74cd05ecf5202cd0d916
Ronald Escobar <ronaldescobarj@gmail.com>
2018-11-23 10:53:21 -0400

    terminado el escenario de atdd ver palito del color del jugador actual en el tablero

--- features/addLine.feature
+++ features/addLine.feature
@@ -6,24 +6,25 @@ Feature: Colocar un palito
     Scenario: ver palito del color del jugador actual en el tablero
         Given estoy en la vista del juego
         When pongo el palito en las coordenadas "1", "1" con la direccion "Horizontal" y es el turno del jugador con el color "azul" y hago clic en "Agregar"
-        Then deberia ver el palito agregado en "1", "1"
+        Then deberia ver el palito agregado en "1", "1" con direccion "horizontal"
             And con el color "azul"
 
     Scenario: mostrar la marca si es que se forma un cuadrado
         Given estoy en la vista del juego
             And hay tres lineas que pueden formar un cuadrado
-        When pongo el palito en las coordenadas "1", "1" con la direccion "Horizontal" y es el turno del jugador con el color "azul", hago clic en "Agregar"
-        Then deberia ver la marca en la posicion "1.5", "1.5", dentro del cuadrado formado
+        When pongo el palito en las coordenadas "1", "1" con la direccion "Horizontal" y es el turno del jugador con el color "azul" y hago clic en "Agregar"
+        Then deberia ver la marca en la posicion "1.5", "1.5"
+            And con el color "azul" dentro del cuadrado formado
 
     Scenario: mantener el turno si es que se forma un cuadrado
         Given estoy en la vista del juego
             And hay tres lineas que pueden formar un cuadrado
-        When pongo el palito en las coordenadas "1", "1" con la direccion "Horizontal" y es el turno del jugador con el color "azul" hago clic en "Agregar"
+        When pongo el palito en las coordenadas "1", "1" con la direccion "Horizontal" y es el turno del jugador con el color "azul" y hago clic en "Agregar"
         Then deberia seguir siendo el turno del jugador con el color "azul"
 
     Scenario: actualizar puntaje si es que se forma un cuadrado
         Given estoy en la vista del juego
             And el puntaje inicial es "0"
             And hay tres lineas que pueden formar un cuadrado
-        When pongo el palito en las coordenadas "1", "1" con la direccion "Horizontal" y es el turno del jugador con el color "azul" hago clic en "Agregar"
+        When pongo el palito en las coordenadas "1", "1" con la direccion "Horizontal" y es el turno del jugador con el color "azul" y hago clic en "Agregar"
         Then deberia actualizarse el puntaje del jugador "azul" a "100"
--- features/step_definitions/addLine_steps.rb
+++ features/step_definitions/addLine_steps.rb
@@ -1,38 +1,40 @@
+require '././lib/LineFunctions'
+
+$line_functions = LineFunctions.new
+$line_id = ""
+
 Given("estoy en la vista del juego") do
-    visit('/game')
+  visit('/game')
 end
 
 When("pongo el palito en las coordenadas {string}, {string} con la direccion {string} y es el turno del jugador con el color {string} y hago clic en {string}") do |x, y, direccion, color, agregar|
-  fill_in('x', with: x.to_i )
+  fill_in('x', with: x.to_i)
   fill_in('y', with: y.to_i)
   select direccion, :from => "direction"
   click_on(agregar)
 end
 
-Then("deberia ver el palito agregado en {string}, {string}") do |string, string2|
-  expect(page.find("#l100_100_200_100")[:style]).to have_content("opacity: 1")
+Then("deberia ver el palito agregado en {string}, {string} con direccion {string}") do |x, y, direccion|
+  $line_id = $line_functions.generate_id(x.to_i*100, y.to_i*100, direccion)
+  expect(page.find("#" + $line_id)[:style]).to have_content("opacity: 1")
 end
 
 Then("con el color {string}") do |color|
   if color == "azul"
     color = "#354698"
   end
-  expect(page.find("#l100_100_200_100")[:stroke]).to have_content("#354698")
+  expect(page.find("#" + $line_id)[:stroke]).to have_content(color)
 end
 
 Given("hay tres lineas que pueden formar un cuadrado") do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("pongo el palito en las coordenadas {string}, {string} con la direccion {string} y es el turno del jugador con el color {string}, hago clic en {string}") do |string, string2, string3, string4, string5|
+Then("deberia ver la marca en la posicion {string}, {string}") do |string, string2|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then("deberia ver la marca en la posicion {string}, {string}, dentro del cuadrado formado") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("pongo el palito en las coordenadas {string}, {string} con la direccion {string} y es el turno del jugador con el color {string} hago clic en {string}") do |string, string2, string3, string4, string5|
+Then("con el color {string} dentro del cuadrado formado") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -41,11 +43,6 @@ Then("deberia seguir siendo el turno del jugador con el color {string}") do |str
 end
 
 Given("el puntaje inicial es {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("pongo el palito en las coordenadas {string}, {string} con la direccion {string} y es el turno del jugador con el color
-{string} hago clic en {string}") do |string, string2, string3, string4, string5|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
